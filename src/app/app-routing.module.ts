7a0d0b7f29f4bddbb2ebdd62d6be37492677c657
mrlanu <mrlanu@gmail.com>
2020-01-28 22:13:12 -0600

    feat: add headerComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HeaderComponent} from './navigation/header/header.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome-page', component: HeaderComponent},
+  {path: '**', redirectTo: '/welcome-page'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
